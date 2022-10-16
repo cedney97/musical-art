895bf5b10c51aea93d9d2fc48453df5df1432f31
cedney97 <c.edney@wustl.edu>
2022-10-16 01:17:10 -0500

    Alrighty, it's translating to a few shapes now

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 var canvas = document.querySelector(".drawing-canvas")
 var context = canvas.getContext("2d")
-context.fillStyle = "#000000"
-context.fillRect(5, 5, canvas.width - 10, canvas.height - 10)
 
 var playButton = document.querySelector(".play-btn")
 var infoButton = document.querySelector(".info-btn")
@@ -42,7 +40,7 @@ infoButton.onclick = function () {
 //---------//
 
 let noteNames = ["c", "c#", "d", "d#", "e", "f", "f#", "g", "g#", "a", "a#", "b"]
-let colors = ["#ff0200", "#ff514f","#ff7f0b", "#ff9933", "#fdff00", "#7fff00", "#09ff00", "#027fff", "#1d00ff", "#5700bf", "#7f00ff", "#d93973"]
+let colors = ["#ff0200", "#ff514f", "#ff7f0b", "#ff9933", "#fdff00", "#7fff00", "#09ff00", "#027fff", "#1d00ff", "#5700bf", "#7f00ff", "#d93973"]
 let essentiaExtractor;
 
 let audioData;
@@ -59,14 +57,24 @@ let hopSize = 2048;
 const uploadToPlayer = () => {
     var file = document.querySelector(".file-select").files[0]
     var reader = new FileReader();
-    reader.onload = function(e) {
+    reader.onload = function (e) {
         document.getElementById("audio-player").src = e.target.result
     }
     reader.readAsDataURL(file)
 }
 
+// https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
+
 async function onClickFeatureExtractor() {
-    audioData = await essentiaExtractor.getAudioChannelDataFromURL(document.getElementById("audio-player").src, audioCtx, 0) 
+    audioData = await essentiaExtractor.getAudioChannelDataFromURL(document.getElementById("audio-player").src, audioCtx, 0)
 
     if (isComputed) {
         plotChroma.destroy();
@@ -127,39 +135,80 @@ async function onClickFeatureExtractor() {
         var intervals = chord.voicing()
         var randX = Math.floor(Math.random() * canvas.width)
         var randY = Math.floor(Math.random() * canvas.height)
-        var color = colors[root.chroma()]
+        var colorHex = colors[root.chroma()]
+        var colorRGB = hexToRgb(colorHex)
         // console.log(color + " in " + intervals + " as " + quality)
-        canvas.fillStyle = color
-        canvas.strokeStyle = color
-        
-    }
+        context.fillStyle = colorHex
+        context.strokeStyle = colorHex
 
-    plotChroma.create(
-        hpcpgram,
-        "HPCP Chroma",
-        audioData.length,
-        audioCtx.sampleRate,
-        hopSize
-    );
+        // For first interval
+        var qualityI1 = intervals[1].toString().charAt(0)
+        var numI1 = intervals[1].toString().charAt(1)
+        var qualityI2 = intervals[2].toString().charAt(0)
+        var numI2 = intervals[1].toString().charAt(1)
+
+        // console.log(numI1)
+        switch (qualityI1) {
+            case 'A':
+                console.log("Augmented Interval")
+                context.beginPath();
+                context.arc(randX, randY, numI1 * 10, 0, Math.PI / 3 * numI1, Math.random() > 0.5)
+                context.fill()
+                context.closePath();
+                break;
+            case 'P':
+                console.log("Perfect Interval")
+                break;
+            case 'm':
+                var rotation = Math.random() * Math.PI * 2
+                context.rotate(rotation)
+                var base = numI2 * 25
+                var height = numI1 * 25
+                context.beginPath();
+                context.moveTo(randX - base / 2, randY)
+                context.lineTo(randX, randY + height)
+                context.lineTo(randX + base / 2, randY)
+                context.lineTo(randX - base / 2, randY)
+                if (Math.random() < 0.5) {
+                    context.stroke();
+                } else {
+                    context.fill();
+                }
+                context.closePath();
+                context.rotate(-rotation)
+                console.log("minor Interval")
+                break;
+            case 'M':
+                var rg = context.createRadialGradient(randX, randY, numI1 * 5, randX, randY, numI1 * 11)
+                rg.addColorStop(0, 'rgba(' + colorRGB.r  + ', ' + colorRGB.b + ', ' + colorRGB.g + ', 1)')
+                rg.addColorStop(1, 'rgba(' + colorRGB.r  + ', ' + colorRGB.b + ', ' + colorRGB.g + ', 0)')
+                context.fillStyle = rg
+
+                context.beginPath();
+                context.fillRect(randX - (numI1 * 75) / 2, randY - (numI1 * 50) / 2, numI1 * 75, numI1 * 50);
+                console.log("Major Interval")
+                context.closePath();
+                break;
+            case 'd':
+                
+                console.log("diminished Interval")
+                break;
+            default:
+                console.log("No Interval?")
+        }
+    }
     isComputed = true;
 }
 
-$(document).ready(function() {
-    plotChroma = new EssentiaPlot.PlotHeatmap(
-        Plotly,
-        plotContainerId,
-        "chroma",
-        EssentiaPlot.LayoutChromaPlot
-    );
-
-    EssentiaWASM().then(async function(WasmModule) {
+$(document).ready(function () {
+    EssentiaWASM().then(async function (WasmModule) {
         essentiaExtractor = new EssentiaExtractor(WasmModule);
 
-        $("#log-div").html (
+        $("#log-div").html(
             "<h5> essentia-" + essentiaExtractor.version + " wasm backend loaded ..."
         );
-        
-        $("#log-div").append (
+
+        $("#log-div").append(
             '<button id="btn" class="ui white inverted button"> Compute HPCP Chroma </button>'
         )
 
